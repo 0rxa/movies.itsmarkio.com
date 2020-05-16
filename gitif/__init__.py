7b30b1757f704fc9a19afa54fec51f9ff2f74e9e
reiarifi <reiarifi@gmail.com>
2020-05-16 18:21:33 +0000

    gitif

--- gitif/__init__.py
+++ gitif/__init__.py
@@ -1,16 +1,22 @@
 from git import Repo
-import os
+from config import Config
 
-basedir = os.environ.get("PWD")
-basedir = basedir + "/../infrastructure/"
+import os
+import hashlib
+import hmac
+import base64
 
 class Git():
-    repos = [ 'web', 'relay' ]
-    def __init__(self, repo_name):
-        self.repo = Repo(basedir + repo_name)
+    basedir = Config.basedir
+    key = Config.key
 
+    def __init__(self, repo_name):
+        self.repo = Repo(self.basedir + repo_name)
 
     def pull(self, branch='master'):
         self.repo.remotes.origin.pull(branch)
 
-        
+    def verify(self, signature, payload):
+        result = str(hmac.new(bytes(self.key, 'utf-8'), payload, hashlib.sha1).hexdigest())
+        print(result)
+        return hmac.compare(signature, result)
