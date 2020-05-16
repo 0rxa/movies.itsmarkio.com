37b518ebb17df023593cb0a957dfff1fd3bf904e
reiarifi <reiarifi@gmail.com>
2020-05-16 18:21:56 +0000

    init

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from flask import Flask, request
 from flask import Response
 from gitif import Git
+from config import Config
+
+repos = Config.repos
 
 app = Flask(__name__)
-
-repos = [ 'web', 'relay' ]
 
 @app.route('/pull/<string:repo>', methods=['GET', 'POST'])
 def pullrepo(repo):
     if not repo in repos:
         return Response(status=404)
+    git = Git(repo)
     branch = request.args.get('branch')
+        return Response(status=403)
     Git(repo).pull(branch)
     return Response(status=200)
 
